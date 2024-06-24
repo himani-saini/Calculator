9fd75c531dc0577206e8896f51efe1e969758772
sainihimani2003 <sainihimani2003@gmail.com>
2024-06-24 12:21:27 +0530

    changed operator logic

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="p-3" style="max-width: 400px; margin: 50px auto; background: #234;">
       <div class="w-full rounded m-1 p-3 text-end lead font-weight-bold text-white bg-vue-dark ">
-        {{ calculatorValue || 0 }}
+        {{ calculatorValue || 0}}
       </div>
   
-      <div class="row no-gutters">
+      <div class="row g-0">
         <div class="col-3" v-for="n in calculatorElements" :key="n">
           <div class="lead text-white text-center bg-vue-dark py-3 m-1 rounded hover-class"
-            :class="{'bg-vue-green':['C','*','/','-','+','%','='].includes(n)}"
+            :class="{'bg-vue-green':['C','*','/','-','+','%','=']}"
             @click="action(n)"
           >
             {{ n }}
@@ -49,15 +49,15 @@
         if (['/', '+', '-', '*'].includes(n)) {
           this.operator = n;
           this.previousCalculatorValue = this.calculatorValue;
-          this.calculatorValue = '';
+          this.calculatorValue = this.operator;
         }
   
-        if (n === '=') {
+        if (n ==='=') {
           if (this.operator && this.previousCalculatorValue !== '') {
             if (this.operator === '/' && this.calculatorValue === '0') {
-              this.calculatorValue = 'Error';
+              this.calculatorValue = 'error';
             } else {
-              this.calculatorValue = eval(this.previousCalculatorValue + this.operator + this.calculatorValue).toString();
+              this.calculatorValue = eval(this.previousCalculatorValue + this.calculatorValue).toString();
             }
             this.previousCalculatorValue = '';
             this.operator = null;
